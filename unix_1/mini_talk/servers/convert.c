574dbadbd92a35bdb5afc9eadb640dda23260cdd
glasset <cirasa@glasset.(none)>
2014-02-02 20:00:16 +0100

    fctnl

---
+++ unix_1/mini_talk/servers/convert.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glasset <glasset@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/02 17:51:10 by glasset           #+#    #+#             */
+/*   Updated: 2014/02/02 19:15:08 by glasset          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include "server.h"
+
+void			convert(void)
+{
+	int			c;
+	int			puiss;
+	char		d;
+
+	puiss = 1;
+	c = 0;
+	while((env->oct) > 0)
+	{
+		c = (env->onechar[env->oct - 1] * puiss) + c;
+		puiss = puiss * 2;
+		env->oct--;
+	}
+	env->oct = 0;
+	d = c;
+	write (1, &d, 1);
+}
+
