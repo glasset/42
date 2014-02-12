562ced1e8a50a199244d4156642ff48e000be856
Guillaume LASSET <glasset@e3r12p2.42.fr>
2014-02-12 18:21:15 +0100

    funart

---
+++ infographie_1/init.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glasset <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/06 10:38:29 by glasset           #+#    #+#             */
+/*   Updated: 2014/02/12 14:40:14 by glasset          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <math.h>
+#include "rtv.h"
+#include <stdio.h>
+
+void		print_px(t_ray *ray, void *img, t_vec *index)
+{
+	t_vec	spheres;
+	t_vec	plans;
+	double	a;
+	
+	spheres = sphere(ray);
+	plans = plan(ray);
+	if (plans.x > -1.0  && spheres.z != -1.0 && plans.x < spheres.x)
+		a = plans.x;
+	else
+		a = spheres.x + 1.0;
+	if (spheres.z == -1.0 && plans.x == -1.0)
+		put_px_to_img(img, index->x, index->y, 200, 0, 0);
+	else if (spheres.x <= a && spheres.z != -1.0 )
+		put_px_to_img(img, index->x, index->y, spheres.y * 100 * a, spheres.y * 100, spheres.y * 150);
+	else
+		put_px_to_img(img, index->x, index->y, plans.y *150 * plans.x, plans.y * 50, plans.y * 100);
+}
+
+void		init_obj(t_ray *ray, char *str)
+{
+	int		fd;
+	int		i;
+	char	*line;
+	char	**tmp;
+
+	i = 0;
+	ray->obj = (t_obj *)malloc(sizeof(t_obj));
+	fd = open(str, O_RDONLY);
+	if (fd == -1)
+		exit(0);
+	get_next_line(fd, &line);
+	ray->size_obj_s = ft_atoi(line);
+	free(line);
+	while (i < ray->size_obj_s)
+	{
+		get_next_line(fd, &line);
+		tmp = ft_strsplit(line, '|');
+		ray->obj[i].point.x = ft_atoi(tmp[0]) + 0.0;
+		ray->obj[i].point.y = ft_atoi(tmp[1]) + 0.0;
+		ray->obj[i].point.z = ft_atoi(tmp[2]) + 0.0;
+		ray->obj[i++].o_r = ft_atoi(tmp[3]) + 0.0;
+		free(line);
+	}
+	init_plans(ray, fd);
+	close(fd);
+}
+
