630e8c80cbf5ce6c20f344e17ea1de3cbb92a112
glasset <cirasa@glasset.home>
2014-03-03 13:47:30 +0100

    parse

--- infographie_1/rtv/sources/init_scene.c
+++ infographie_1/rtv/sources/init_scene.c
@@ -6,43 +6,57 @@
 /*   By: glasset <glasset@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/01 17:56:47 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/01 19:48:26 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/03 13:46:58 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "ray_tracer.h"
+#include "my.h"
 
-void			init_scene(t_env *e, char *path)
+int			init_s(t_env *e, char *str, int m)
 {
-	(void)path;
-	e->nb_mesh = 2;
-	e->meshes = (t_mesh *)malloc(sizeof(t_mesh) * e->nb_mesh);
-	e->meshes[0].type = T_SPHERE;
-	e->meshes[0].prim.sphere.radius = 1;
-	e->meshes[0].prim.sphere.pos = init_vec(0, 0, 0);
-	e->meshes[0].color.r = 0.0;
-	e->meshes[0].color.g = 0.2;
-	e->meshes[0].color.b = 0.7;
-	e->meshes[0].diff = 1.0;
-	e->meshes[1].type = T_SPHERE;
-	e->meshes[1].prim.sphere.radius = 3;
-	e->meshes[1].prim.sphere.pos = init_vec(-5, -5, -8);
-	e->meshes[1].color.r = 0.7;
-	e->meshes[1].color.g = 0.0;
-	e->meshes[1].color.b = 0.7;
-	e->meshes[1].diff = 0.8;
-	e->cam.pos = init_vec(0, 0, 40);
-	e->look_at_point = init_vec(0, 0, 0);
-	e->ambient = 0.1;
-	e->nb_light = 2;
-	e->lights = (t_light *)malloc(sizeof(t_light) * e->nb_light);
+	int		i;
+	char	*s;
+	char	**tmp;
+
+	i = 0;
+	while (str[i] < '0' && str[i] > '9')
+		i++;
+	s = ft_strsub(str, i, ft_strlen(str));
+	tmp = ft_strsplit(s, '[');
+	e->meshes[m].type = T_SPHERE;
+	e->meshes[m].prim.sphere.radius = ft_atoi(tmp[4]);
+	e->meshes[m].prim.sphere.pos = init_vec(ft_atoi(tmp[1]),
+	ft_atoi(tmp[2]), ft_atoi(tmp[3]));
+	e->meshes[m].color.r = 0.7;
+	e->meshes[m].color.g = 0.7;
+	e->meshes[m].color.b = 0.7;
+	e->meshes[m].diff = 1.0;
+	return (m + 1);
+}
+
+int			init_l(t_env *e, char *str, int m)
+{
+	int		i;
+	char	*s;
+	char	**tmp;
+
+	i = 0;
+	while (str[i] < '0' && str[i] > '9')
+		i++;
+	s = ft_strsub(str, i, ft_strlen(str));
+	tmp = ft_strsplit(s, '[');
 	e->lights[0].color.r = 0.7;
 	e->lights[0].color.g = 0.7;
 	e->lights[0].color.b = 0.7;
-	e->lights[0].pos = init_vec(40, 40, 40);
-	e->lights[1].color.r = 0.7;
-	e->lights[1].color.g = 0.7;
-	e->lights[1].color.b = 0.7;
-	e->lights[1].pos = init_vec(40, 40, 50);
+	e->lights[0].pos = init_vec(ft_atoi(tmp[0]),
+	ft_atoi(tmp[1]), ft_atoi(tmp[2]));
+	return (m);
+}
+
+void			init_scene(t_env *e, char *path)
+{
+	e->ambient = 0.1;
+	parse(e, path);
 }
--- infographie_1/rtv/sources/parser.c
+++ infographie_1/rtv/sources/parser.c
@@ -6,48 +6,81 @@
 /*   By: glasset <glasset@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/01 17:47:43 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/01 19:55:21 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/03 13:46:59 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "my.h"
+#include "ray_tracer.h"
+#include "gnl.h"
+#include <stdlib.h>
+#include <fcntl.h>
 
 f				*funct(void)
 {
 	f			*ft;
 	ft = malloc(sizeof(ft) * 8);
-	ft[0] = &comment;
-	ft[1] = &base;
-	ft[2] = &init_c;
-	ft[3] = &init_s;
+	ft[0] = &comment;//k
+//	ft[1] = &init_c;
+	ft[2] = &init_s;
+//	ft[3] = &init_p;
+	ft[4] = &init_l;
+	ft[5] = &nbmesch; //ok
+	ft[6] = &nblight;//k
+	ft[7] = &pov;//k
+	ft[8] = &lookat;//k
 	return (ft);
+}
+
+int				whoinit(char *str)
+{
+	int			i;
+
+	i  = -1;
+	if (str[1] == 'P')
+		return (7);
+	if (str[1] == 'L')
+		return (8);
+	while (str[++i])
+	{
+		if (str[i] == 'l' && str[i + 1] == 'i')
+			return (6);
+		if (str[i] == 'o' && str[i + 1] == 'b')
+			return (5);
+	}
+	return (0);
 }
 
 int				check_line(char *str)
 {
 	char		*opt;
 	int			i;
-	f			*ft;
 
-	ft = funct();
-	i = -1;
-	opt = "|CSPLC"
+i = -1;
+	opt = "cSPLC";
 	while (opt[++i])
 	{
+		if (str[0] == '|')
+			return (whoinit(str));
 		if (opt[i] == str[0])
 			return (i + 1);
 	}
 	return (0);
+}
 
 void			parse(t_env *e, char *path)
 {
 	char		*line;
 	int			fd;
 	int			current_m;
+	f			*ft;
 
+
+	ft = funct();
 	current_m = 0;
 	fd = open(path, O_RDONLY);
 	while (get_next_line(fd, &line))
 	{
-		current_m = f[check_line(line)](e, line, current_m);
+		current_m = ft[check_line(line)](e, line, current_m);
 		free(line);
 	}
 }
