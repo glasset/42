0fba1158e156e6d18adf54261703c75c160f5316
Guillaume LASSET <glasset@e3r11p1.42.fr>
2014-03-12 16:06:10 +0100

    Ok parser

--- algorithmie_1/lemin/lemin.h
+++ algorithmie_1/lemin/lemin.h
@@ -6,7 +6,7 @@
 /*   By: glasset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/18 13:03:55 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/11 16:46:33 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/12 16:05:44 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ typedef struct			s_env
 t_lst			*lst_new(void);
 t_lst			*lst_add_end(t_lst *l_xm, char *name, int *xy, int pos);
 void			parser(t_env *e);
+void			print(t_env *e);
 void			error(void);
 
 #endif /* !LEMIN_H */
--- algorithmie_1/lemin/main.c
+++ algorithmie_1/lemin/main.c
@@ -6,7 +6,7 @@
 /*   By: glasset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/18 12:59:06 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/12 15:19:12 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/12 16:05:43 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -24,29 +24,9 @@ void			error()
 int				main(void)
 {
 	t_env		e;
-	size_t			i;
-	size_t			s;
-	i = 0;
+
 	e.room = lst_new();
 	parser(&e);
-	printf("%d ants\n\n", e.ants);
-	while(i < e.room->len)
-	{
-		printf("name: %s\npos: %d\nx:%d|y:%d\n", e.room->start->name,
-				e.room->start->pos, e.room->start->xy[0], e.room->start->xy[1]);
-		e.room->start = e.room->start->next;
-		if (e.room->start->tube)
-		{
-			s = 0;
-			while(s < e.room->start->tube->len)
-			{
-				printf("liaison: %s\n", e.room->start->tube->start->name);
-				e.room->start->tube->start = e.room->start->tube->start->next;
-				s++;
-			}
-		}
-		i++;
-		write(1, "\n", 1);
-	}
+	print(&e);
 	return (0);
 }
