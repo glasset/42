9bcffc6c28f5793c85cb2108a0fe56f373873959
Guillaume LASSET <glasset@e3r11p1.42.fr>
2014-03-15 15:14:03 +0100

    lemin

--- algorithmie_1/lemin/djidji.c
+++ algorithmie_1/lemin/djidji.c
@@ -6,7 +6,7 @@
 /*   By: glasset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/13 12:02:14 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/13 13:55:37 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/15 15:13:52 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -24,7 +24,7 @@ void			seek_room(t_env *e, int pos, char *name)
 	{
 		if (name == NULL)
 			exit(0);
-		while (!ft_strcmp(e->room->start->name, name))
+		while (ft_strcmp(e->room->start->name, name))
 			e->room->start = e->room->start->next;
 	}
 }
@@ -53,18 +53,9 @@ int				lenght(t_env *e, char *curr, char *next)
 
 int				check_start(t_env *e, int pos)
 {
-	size_t		i;
-
-	i = 0;
 	seek_room(e, pos, "s");
-//	while (i < e->room->start->tube->len)
-//	{
-//		seek_room(e, 0, e->room->start->tube->start->name);
-		if (e->room->start->weight > -1)
+	if (e->room->start->weight > -1)
 			return (1);
-//		seek_room(e, 1, "s");
-		i++;
-//	}
 	return (0);
 }
 
@@ -72,7 +63,7 @@ void			shoort(t_env *e)
 {
 	size_t		i;
 	int			W;
-	char		back[2];
+	char		*back;
 	int			x = 1;
 
 	while (check_start(e, 1) == 0)
@@ -81,24 +72,18 @@ void			shoort(t_env *e)
 		if (x ==1)
 		{
 			seek_room(e, 2, "e");
+			e->room->start->back = "e";
 			e->room->start->weight = 0;
 			x = 0;
 		}
 		while (i < e->room->start->tube->len)
 		{
-			back[0] = e->room->start->name[0];
-			back[1] = '\0';
+			back = e->room->start->name;
 			W = lenght(e, e->room->start->name, e->room->start->tube->start->name);
-//			seek_room(e, 0, e->room->start->tube->start->name);
 			if ( e->room->start->weight == -1 || W < e->room->start->weight)
 			{
 				e->room->start->weight = W;
-				if (e->room->start->back == NULL)
-				{
-				e->room->start->back = malloc(sizeof(char)*2);
-				e->room->start->back[0] = back[0];
-				e->room->start->back[1] = '\0';
-				}
+				e->room->start->back = back;
 			}
 			i++;
 			e->room->start->tube->start = e->room->start->tube->start->next;
--- algorithmie_1/lemin/main.c
+++ algorithmie_1/lemin/main.c
@@ -6,7 +6,7 @@
 /*   By: glasset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/18 12:59:06 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/13 13:55:38 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/15 15:13:53 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -25,13 +25,12 @@ void			print_res(t_env *e)
 {
 
 	seek_room(e, 1, "s");
-//	while (e->room->start->weight != 0)
-//	{
+	while (e->room->start->pos != END)
+	{
 		ft_putstr(e->room->start->name);
 		write(1, "->", 2);
-		ft_putstr(e->room->start->back);
 		seek_room(e, 0, e->room->start->back);
-//	}
+	}
 	ft_putstr(e->room->start->name);
 	write(1, "\n", 1);
 }
