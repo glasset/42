494ee0e0d6eee52ddb1fe3a48a5d7466a06e52f8
Guillaume LASSET <glasset@e3r11p1.42.fr>
2014-03-15 16:08:01 +0100

    try2ok

--- algorithmie_1/lemin/djidji.c
+++ algorithmie_1/lemin/djidji.c
@@ -6,12 +6,29 @@
 /*   By: glasset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/13 12:02:14 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/15 15:13:52 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/15 16:07:50 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 #include <stdlib.h>
 #include "lemin.h"
+
+int				seek_nop(t_env *e, int pos, char *name)
+{
+	t_ch		*s;
+
+	name = name;
+	s = e->room->start;
+	if (pos > 0)
+	{
+		while (s->pos != START)
+			s = s->next;
+	}
+	if (s->weight > -1)
+		return (0);
+	return (1);
+}
+
 
 void			seek_room(t_env *e, int pos, char *name)
 {
@@ -33,42 +50,53 @@ int				lenght(t_env *e, char *curr, char *next)
 {
 	int			x;
 	int			y;
+	t_ch		*tmp;
 
-	while (ft_strcmp(e->room->start->name, curr))
-			e->room->start = e->room->start->next;
-	x = e->room->start->xy[0];
-	y = e->room->start->xy[1];
-	while (ft_strcmp(e->room->start->name, next))
-			e->room->start = e->room->start->next;
-	if (x > e->room->start->xy[0])
-		x -= e->room->start->xy[0];
+	tmp = e->room->start;
+	while (ft_strcmp(tmp->name, curr))
+			tmp = tmp->next;
+	x = tmp->xy[0];
+	y = tmp->xy[1];
+	while (ft_strcmp(tmp->name, next))
+			tmp = tmp->next;
+	if (x > tmp->xy[0])
+		x -= tmp->xy[0];
 	else
-		x = e->room->start->xy[0] - x;
-	if (y > e->room->start->xy[1])
-		y -= e->room->start->xy[1];
+		x = tmp->xy[0] - x;
+	if (y > tmp->xy[1])
+		y -= tmp->xy[1];
 	else
-		y = e->room->start->xy[1] - y;
+		y = tmp->xy[1] - y;
 	return (y + x);
 }
 
-int				check_start(t_env *e, int pos)
+void				update(t_env *e, int w, char *curr, char *next)
 {
-	seek_room(e, pos, "s");
-	if (e->room->start->weight > -1)
-			return (1);
-	return (0);
+	t_ch		*tmp;
+
+	tmp = e->room->start;
+	while (ft_strcmp(tmp->name, next))
+			tmp = tmp->next;
+	if (tmp->back == NULL)
+	{
+		tmp->back = curr;
+		tmp->weight = w;
+	}
+	else if (w < tmp->weight)
+	{
+		tmp->back = curr;
+		tmp->weight = w;
+	}
 }
 
 void			shoort(t_env *e)
 {
 	size_t		i;
 	int			W;
-	char		*back;
 	int			x = 1;
 
-	while (check_start(e, 1) == 0)
+	while (seek_nop(e, START, "s"))
 	{
-		i = 0;
 		if (x ==1)
 		{
 			seek_room(e, 2, "e");
@@ -76,17 +104,15 @@ void			shoort(t_env *e)
 			e->room->start->weight = 0;
 			x = 0;
 		}
+		i = 0;
 		while (i < e->room->start->tube->len)
 		{
-			back = e->room->start->name;
 			W = lenght(e, e->room->start->name, e->room->start->tube->start->name);
-			if ( e->room->start->weight == -1 || W < e->room->start->weight)
-			{
-				e->room->start->weight = W;
-				e->room->start->back = back;
-			}
+			update(e, W, e->room->start->name, e->room->start->tube->start->name);
 			i++;
 			e->room->start->tube->start = e->room->start->tube->start->next;
+			if (i == e->room->start->tube->len)
+				e->room->start = e->room->start->next;
 		}
 	}
 }
