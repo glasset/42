c8329e6c788f4ed0ed8c64aea9c72e52a839341e
glasset <cirasa@glasset.home>
2014-03-26 01:58:55 +0100

    rt

--- infographie_1/dd/ray_tracer/sources/init_light.c
+++ infographie_1/dd/ray_tracer/sources/init_light.c
@@ -6,34 +6,56 @@
 /*   By: glasset <glasset@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/25 14:16:46 by glasset           #+#    #+#             */
-/*   Updated: 2014/03/25 14:49:30 by glasset          ###   ########.fr       */
+/*   Updated: 2014/03/26 01:58:35 by glasset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "parser.h"
 
+static int	light_ign(int c_l, int fd, char *str)
+{
+	char	*line;
+	int		li;
+
+	li = 1;
+	error_p(str, "light ignored check number of light", c_l);
+	while (get_next_line(fd, &line))
+	{
+		if (line[0] == END_OBJ)
+			break ;
+		li++;
+	}
+	return (li);
+}
+
+static int	print_mess(char *line, int c_l, int li, int tmp)
+{
+	if (tmp == -1)
+		error_p(ft_strsub(line, cut_space(line), ft_strlen(line)),
+				"\033[31m[WARNING]\033[0m wrong value", c_l + li);
+	if (tmp == -2)
+	{
+		error_p(ft_strsub(line, cut_space(line), ft_strlen(line)),
+				"\033[31m[ERROR]\033[0m light ignored", c_l + li);
+		free(line);
+		return (-1);
+	}
+	return (0);
+}
+
 int			light(t_env *e, int c_l, int fd, char *str)
 {
 	char		*line;
 	static int	c = 0;
-	int			li;
 	l			*ft;
+	int			li;
 	int			tmp;
 
 	ft = ft_inl();
 	li = 1;
 	if (c == e->nb_light_malloc || e->nb_light_malloc == -1)
-	{
-		error_p(str, "light ignored check number of light", c_l);
-		while (get_next_line(fd, &line))
-		{
-			if (line[0] == END_OBJ)
-				break ;
-			li++;
-		}
-		return (li);
-	}
+		return (light_ign(c_l, fd, str));
 	while (get_next_line(fd, &line))
 	{
 		if (line[0] == END_OBJ)
@@ -45,20 +67,11 @@ int			light(t_env *e, int c_l, int fd, char *str)
 		}
 		if (checkline_light(line) == -1)
 			error_p(ft_strsub(line, cut_space(line), ft_strlen(line)),
-					"\033[31m[WARNING]\033[0m unknown line", li + c_l);
+				"\033[31m[WARNING]\033[0m unknown line", li + c_l);
 		else
 			tmp = ft[checkline_light(line)](e, line, c);
-		if (tmp == -1)
-			error_p(ft_strsub(line, cut_space(line), ft_strlen(line)),
-					"\033[31m[WARNING]\033[0m wrong value", c_l + li);
-		if (tmp == -2)
-		{
-			error_p(ft_strsub(line, cut_space(line), ft_strlen(line)),
-					"\033[31m[ERROR]\033[0m light ignored", c_l + li);
-			free(line);
+		if (print_mess(line, c_l, li, tmp) == -1)
 			return (li);
-		}
-
 		free(line);
 		li++;
 	}
