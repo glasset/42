a2e9f55cd62552db5c58cb767bf20baa0d49af32
Guillaume LASSET <glasset@e3r12p1.42.fr>
2014-04-01 18:29:51 +0200

    php

--- pool_php/j01/ex10/do_op.php
+++ pool_php/j01/ex10/do_op.php
@@ -3,29 +3,17 @@
 if ($argc == 4)
 {
 	$nb = trim($argv[2]);
-	if(!is_numeric(trim($argv[1])))
-	{
-		echo "Incorrect Parameters\n";
-		break;
-	}
-	if(!is_numeric(trim($argv[3])))
-	{
-		echo "Incorrect Parameters\n";
-		break;
-	}
 	$nb = ord($nb);
 	if ($nb == 43)
 		echo $argv[1] + $argv[3]."\n";
 	else if ($nb == 45)
 		echo $argv[1] - $argv[3]."\n";
-	else if ($nb == 42)
+	else if ($nb == 100)
 		echo $argv[1] * $argv[3]."\n";
 	else if ($nb == 47)
 		echo $argv[1] / $argv[3]."\n";
 	else if ($nb == 37)
 		echo $argv[1] % $argv[3]."\n";
-	else
-		echo "Incorrect Parameters\n";
 }
 else
 	echo "Incorrect Parameters\n";
